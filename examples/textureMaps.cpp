d0894135280bb778e502b4f5440c2da69e8289b8
Boris Vassilev <boris.n.vassilev@gmail.com>
2024-01-15 19:16:56 +0200

    some changes dunno

--- examples/textureMaps.cpp
+++ examples/textureMaps.cpp
@@ -8,6 +8,7 @@
 #include <renderer.h>
 #include <asset_manager.h>
 #include <mesh.h>
+#include <entities.h>
 
 using namespace ygl;
 
@@ -26,15 +27,21 @@ void run() {
 
 	Renderer *renderer = scene.registerSystem<Renderer>(&window);
 
-	Texture2d *color	 = new Texture2d("./res/images/stones/albedo.png", TextureType::SRGB8);
-	Texture2d *normal	 = new Texture2d("./res/images/stones/normal.png", TextureType::RGB16F);
-	Texture2d *roughness = new Texture2d("./res/images/stones/roughness.png", TextureType::GREY16F);
-	Texture2d *ao		 = new Texture2d("./res/images/stones/ao.png", TextureType::GREY16F);
+	// Texture2d *color	 = new Texture2d("./res/images/stones/albedo.png", TextureType::DIFFUSE);
+	// Texture2d *normal	 = new Texture2d("./res/images/stones/normal.png", TextureType::NORMAL);
+	// Texture2d *roughness = new Texture2d("./res/images/stones/roughness.png", TextureType::ROUGHNESS);
+	// Texture2d *ao		 = new Texture2d("./res/images/stones/ao.png", TextureType::AO);
 
-	Mesh *modelMesh = new BoxMesh(glm::vec3(1, 1, 1), glm::vec3(20, 20, 20));
+	Texture2d *color	 = new Texture2d("./res/images/cobble/chiseled-cobble_albedo.png", TextureType::DIFFUSE);
+	Texture2d *normal	 = new Texture2d("./res/images/cobble/chiseled-cobble_normal-ogl.png", TextureType::NORMAL);
+	Texture2d *roughness = new Texture2d("./res/images/cobble/chiseled-cobble_roughness.png", TextureType::ROUGHNESS);
+	Texture2d *ao		 = new Texture2d("./res/images/cobble/chiseled-cobble_ao.png", TextureType::AO);
+
+	// Mesh *modelMesh = new BoxMesh(glm::vec3(1, 1, 1), glm::vec3(20, 20, 20));
+	Mesh *modelMesh = new PlaneMesh(glm::vec2(1, 1));
 
 	Entity model = scene.createEntity();
-	scene.addComponent<Transformation>(model, Transformation(glm::vec3(), glm::vec3(0), glm::vec3(1)));
+	scene.addComponent<Transformation>(model, Transformation(glm::vec3(), glm::vec3(glm::pi<float>() / 2,0,0), glm::vec3(1)));
 
 	Material modelMat(glm::vec3(1.0, 0.5, 0.0), 0.02, glm::vec3(0), 1.0, glm::vec3(1.0), 0.0, glm::vec3(1.0), 0.0, 0.4,
 					  0.0, 0.);
@@ -44,7 +51,7 @@ void run() {
 	modelMat.normal_map		   = asman->addTexture(normal, "normal");
 	modelMat.use_normal_map	   = 1.0;
 	modelMat.roughness_map	   = asman->addTexture(roughness, "roughness");
-	modelMat.use_roughness_map = 0.0;
+	modelMat.use_roughness_map = 1.0;
 	modelMat.ao_map			   = asman->addTexture(ao, "ao");
 	modelMat.use_ao_map		   = 1.0;
 
@@ -54,13 +61,16 @@ void run() {
 	modelRenderer.meshIndex		= asman->addMesh(modelMesh, "canonicalCube");
 	scene.addComponent(model, modelRenderer);
 
-	renderer->addLight(Light(Transformation(glm::vec3(0), glm::vec3(1, -.3, 0), glm::vec3(1)), glm::vec3(1., 1., 1.), 3,
-							 Light::Type::DIRECTIONAL));
-	renderer->addLight(Light(Transformation(), glm::vec3(1., 1., 1.), 0.01, Light::Type::AMBIENT));
+	addSkybox(scene, "res/images/blue_photo_studio_4k", ".hdr");
+
+	// renderer->addLight(Light(Transformation(glm::vec3(0), glm::vec3(1, -.3, 0), glm::vec3(1)), glm::vec3(1., 1., 1.), 3,
+	// 						 Light::Type::DIRECTIONAL));
+	// renderer->addLight(Light(Transformation(), glm::vec3(1., 1., 1.), 0.01, Light::Type::AMBIENT));
 
 	renderer->loadData();
 
 	controller.speed = 0.6;
+	int editMaterialIndex = 0;
 
 	glClearColor(0, 0, 0, 1);
 	while (!window.shouldClose()) {
@@ -76,6 +86,12 @@ void run() {
 		cam.update();
 
 		renderer->doWork();
+
+		ImGui::Begin("Material Properties");
+		ImGui::InputInt("Material ID", &editMaterialIndex);
+		ImGui::End();
+		renderer->getMaterial(editMaterialIndex).drawImGui();
+		renderer->loadData();
 
 		window.swapBuffers();
 	}
